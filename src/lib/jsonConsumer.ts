518845e20716b9883747702c03da335017345dda
BPPH <operationbumdrive1@gmail.com>
2025-09-25 18:53:56 +0100

    feat: lick JSON

--- src/lib/jsonConsumer.ts
+++ src/lib/jsonConsumer.ts
@@ -3,7 +3,7 @@ import {lexarg} from "./lexarg";
 
 const DIGITS = '0123456789';
 
-function readJSONNumber(str: string): number {
+function readJSONNumber(str: string) {
     let ret = '';
 
     const minus = str.charAt(0) == '-';
@@ -45,35 +45,42 @@ function readJSONNumber(str: string): number {
                 subRet += sub.charAt(0);
                 sub = sub.slice(1);
             }
-
-            return subRet;
         }
+        return subRet;
+    }
 
+    if (str.charAt(0) === '0') {
+        ret += '0';
+        str = str.slice(1);
+    } else if ('123456789'.includes(str.charAt(0))) {
+        while (DIGITS.includes(str.charAt(0))) {
+            ret += str.charAt(0);
+            str = str.slice(1);
+        }
     }
 
     let [frac, str2] = readFraction(str);
     ret += frac;
     ret += readExponent(str2);
 
-    return JSON.parse(ret);
+    return ret;
 }
 
 /**
- * Reads a JSON string and stops at the natural end (i.e. when brackets close, or when quotes end, etc.)
- * @param str String that would usually go in JSON.parse()
- * @param i Starting index. Defaults at 0.
+ * Find natural end of JSON
+ * @param str
+ * @param i
  */
-export function consumeJSON(str: string, i: number = 0) {
-    // named by ChatGPT
+export function lickJSON(str: string, i: number) {
     const section = str.slice(i);
     if (section.startsWith('true')) {
-        return true;
+        return i + 4;
     } else if (section.startsWith('false')) {
-        return false;
-    } else if (section.endsWith('null')) {
-        return null;
+        return i + 5;
+    } else if (section.startsWith('null')) {
+        return i + 4;
     } else if ('0123456789.-'.includes(section.charAt(0))) {
-        return readJSONNumber(section);
+        return i + readJSONNumber(section).length;
     }
 
     let depth = 0;
@@ -81,8 +88,8 @@ export function consumeJSON(str: string, i: number = 0) {
     let outString = true;
     let unescaped = true;
 
-    for (let i = 0; i < section.length; i++) {
-        const char = section.charAt(i);
+    for (let j = 0; j < section.length; j++) {
+        const char = section.charAt(j);
 
         JSONText += char;
 
@@ -113,14 +120,37 @@ export function consumeJSON(str: string, i: number = 0) {
         }
 
         if (depth === 0 && JSONText.trim().length > 1) {
-            return JSON.parse(JSONText.trim());
+            return i + j + 1;
         }
     }
     throw `Unclosed JSON Error; read ${JSONText}`;
 }
 
+/**
+ * Reads a JSON string and stops at the natural end (i.e. when brackets close, or when quotes end, etc.)
+ * @param str String that would usually go in JSON.parse()
+ * @param i Starting index. Defaults at 0.
+ */
+export function consumeJSON(str: string, i: number = 0) {
+    // named by ChatGPT
+    const end = lickJSON(str, i);
+    return JSON.parse(str.slice(i, end));
+}
+
 
 if (require.main === module) {
+    console.log(consumeJSON(`true a`));
+    console.log(consumeJSON(`false a`));
+    console.log(consumeJSON(`null a`));
+    console.log(consumeJSON(`1 a`));
+    console.log(consumeJSON(`-1 a`));
+    console.log(consumeJSON(`-1.5 a`));
+    console.log(consumeJSON(`1e+20 a`));
+    console.log(consumeJSON(`-1e+20 a`));
+    console.log(consumeJSON(`-1e2 a`));
+    console.log(consumeJSON(`1.20 a`));
+    console.log(consumeJSON(`1.20e2 a`));
+    console.log(consumeJSON(`1.20e2 a`));
     console.log(consumeJSON(`"hey there" idk man`));
     console.log(consumeJSON(`{"hey there": 123, "aa": [2, {}, [1, 2]]} idk man`));
 }
